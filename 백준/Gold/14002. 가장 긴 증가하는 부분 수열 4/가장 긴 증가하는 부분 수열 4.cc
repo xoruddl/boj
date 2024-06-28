3bed7d8135d19c880f00e731715cf28fe6318ccd
xoruddl <161710171+xoruddl@users.noreply.github.com>
2024-06-28 15:29:31 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -3,40 +3,48 @@
 
 using namespace std;
 
+int Asize;  // 수열 크기
+int A[1001];    // 수열
+int len_dp[1001];   // 가장 긴 수열의 길이 디피
+int ans[1001];
+
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int N, i, j;
-    int A[1000], len[1000], ans[1000];
-    cin >> N;
-    for (i = 0; i < N; i++)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> Asize;
+    for (int i = 0; i < Asize; i++) {
         cin >> A[i];
-    // LIS 구하기
-    for (i = 0; i < N; i++) {
-        len[i] = 1;
-        for (j = 0; j < i; j++) {
-            if (A[i] > A[j])
-                len[i] = max(len[i], len[j] + 1);
+    }
+
+    for (int i = 0; i < Asize; i++) {
+        len_dp[i] = 1;
+        for (int j = 0; j < i; j++) {
+            if (A[i] > A[j]) {
+                len_dp[i] = max(len_dp[i], len_dp[j] + 1);
+            }
         }
     }
-    // 길이가 가장 긴 곳의 위치 찾기
+
     int maxlen = 0, maxidx;
-    for (i = 0; i < N; i++)
-        if (len[i] > maxlen) {
-            maxlen = len[i];
+    for (int i = 0; i < Asize; i++) {
+        if (len_dp[i] > maxlen) {
+            maxlen = len_dp[i];
             maxidx = i;
         }
-    // 경로 추적
+    }
+
     ans[0] = A[maxidx];
     int l = maxlen - 1;
-    for (i = maxidx - 1, j = 0; i >= 0; i--) {
-        if (len[i] == l && A[i] < ans[j]) {
+    for (int i = maxidx - 1, j = 0; i >= 0; i--) {
+        if (len_dp[i] == l && A[i] < ans[j]) {
             ans[++j] = A[i];
             l--;
         }
     }
-    cout << maxlen << "\n";
-    for (i = maxlen - 1; i >= 0; i--)
-        cout << ans[i] << ' ';
+    cout << maxlen << '\n';
+    for (int i = maxlen - 1; i >= 0; i--) {
+        cout << ans[i] << " ";
+    }
+    return 0;
 }
