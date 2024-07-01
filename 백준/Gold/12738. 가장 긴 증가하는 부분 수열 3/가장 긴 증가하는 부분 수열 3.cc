7f59dd9870e779720020fffda18c5ca15524dee6
xoruddl <161710171+xoruddl@users.noreply.github.com>
2024-07-01 14:39:01 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 172 ms, Memory: 12200 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int A[1000001];
+vector<int> LIS;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (LIS.size() == 0 || LIS[LIS.size() - 1] < A[i]) LIS.push_back(A[i]);
+        else {
+            int left = 0;
+            int right = LIS.size() - 1;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (LIS[mid] >= A[i]) {
+                    right = mid;
+                }
+                else {
+                    left = mid + 1;
+                }
+            }
+            LIS[left] = A[i];
+        }
+    }
+    cout << LIS.size() << '\n';
+    return 0;
+}
