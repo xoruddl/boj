3e32296d00f35970a2ee369f2ff35c58596aa539
xoruddl <161710171+xoruddl@users.noreply.github.com>
2024-07-19 20:57:38 +0900

    [Gold III] Title: 나만 안되는 연애, Time: 4 ms, Memory: 2340 KB -BaekjoonHub

--- 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.cc
+++ 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.cc
@@ -4,19 +4,20 @@
 
 using namespace std;
 
-int N, M, p[1001];
+int N, M;
+int parent[1001];
 char MW[1001];
-vector<pair<pair<int, int>, int>> edge;
+vector<pair<pair<int, int>, int>> Edge;
 
 int find(int x) {
-    if (p[x] == x) return x;
-    else return p[x] = find(p[x]);
+    if (parent[x] == x) return x;
+    else return parent[x] = find(parent[x]);
 }
 
 void uni(int x, int y) {
     x = find(x);
     y = find(y);
-    p[y] = x;
+    parent[y] = x;
 }
 
 bool sameParent(int x, int y) {
@@ -33,19 +34,18 @@ bool cmp(pair<pair<int, int>, int> p1, pair<pair<int, int>, int> p2) {
 void Input() {
     cin >> N >> M;
     for (int i = 1; i <= N; i++) {
-        p[i] = i;   // 부모 베열 초기화
-        cin >> MW[i];   // 학교 입력
+        parent[i] = i;
+        cin >> MW[i];
     }
 
     for (int i = 0; i < M; i++) {
         int u, v, d;
         cin >> u >> v >> d;
         if (MW[u] != MW[v]) {
-            edge.push_back({{u, v}, d});
+            Edge.push_back({{u, v}, d});
         }
     }
-
-    sort(edge.begin(), edge.end(), cmp);    // 최소비용으로 정렬
+    sort(Edge.begin(), Edge.end(), cmp);
 }
 
 int main() {
@@ -55,16 +55,16 @@ int main() {
     Input();
 
     int ans = 0, cnt = 0;
-    for (auto V: edge) {
-        int u = V.first.first;
-        int v = V.first.second;
-        int d = V.second;
+    for (auto E: Edge) {
+        int u = E.first.first;
+        int v = E.first.second;
+        int d = E.second;
 
-        if (!sameParent(u, v)) {    // 연결되어있지 않다면
-            uni(u, v);
+        if (!sameParent(u, v)) {
             ans += d;
+            uni(u, v);
             cnt++;
-            if (cnt == N - 1) { // 다 연결되었다면
+            if (cnt == N - 1) {
                 cout << ans;
                 return 0;
             }
