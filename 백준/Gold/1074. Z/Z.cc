c6e17993530552708b670e615d14651aa2636689
etakyung <161710171+xoruddl@users.noreply.github.com>
2024-10-11 23:53:30 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.cc
+++ 백준/Gold/1074. Z/Z.cc
@@ -1,31 +1,20 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
-int N, r, c;
-int ans;
+int fc(int n, int r, int c) {
+    if (n == 0) return 0;
+    int half = 1 << (n - 1);
 
-void fc(int x, int y, int size) {
-    if (c == x && r == y) { // 열과 행 일치
-        cout << ans;
-        return;
-    }
-    else if (c < x + size && r < y + size && c >= x && r >= y) {
-        fc(x, y, size / 2);
-        fc(x + size / 2, y, size / 2);
-        fc(x, y + size / 2, size / 2);
-        fc(x + size / 2, y + size / 2, size / 2);
-    }
-    else {
-        ans += size * size;
-    }
+    if (r < half && c < half) return fc(n - 1, r, c);
+    if (r < half && c >= half) return half * half + fc(n - 1, r, c - half);
+    if (r >= half && c < half) return 2 * half * half + fc(n - 1, r - half, c);
+    return 3 * half * half + fc(n - 1, r - half, c - half);
 }
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-    cin >> N >> r >> c;
-    fc(0, 0, pow(2, N));
-    return 0;
+    int n, r, c;
+    cin >> n >> r >> c;
+    cout << fc(n, r, c);
 }
