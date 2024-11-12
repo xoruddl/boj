3931c1009f1d612470ed9614bc3378e3da7f5c36
etakyung <161710171+xoruddl@users.noreply.github.com>
2024-11-12 16:13:20 +0900

    [Gold V] Title: 배열 탈출, Time: 344 ms, Memory: 40696 KB -BaekjoonHub

--- 백준/Gold/11909. 배열 탈출/배열 탈출.cc
+++ 백준/Gold/11909. 배열 탈출/배열 탈출.cc
@@ -1,59 +1,35 @@
 #include <iostream>
 #include <algorithm>
-#include <queue>
 
 using namespace std;
 
 int n;
 int MAP[2225][2225];
 int dp[2225][2225];
-int dx[] = {1, 0};
-int dy[] = {0, 1};
-
-void fc() {
-    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-    pq.push({0, {1, 1}});
-
-    while (!pq.empty()) {
-        int x = pq.top().second.first;
-        int y = pq.top().second.second;
-        int cost = pq.top().first;
-        pq.pop();
-
-        for (int i = 0; i < 2; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            int ncost = cost;
-
-            if (nx < 1 || ny < 1 || nx > n || ny > n) continue;
-            if (MAP[nx][ny] >= MAP[x][y]) {
-                ncost += (MAP[nx][ny] - MAP[x][y] + 1);
-            }
-            if (ncost >= dp[nx][ny]) {
-                continue;
-            }
-            dp[nx][ny] = ncost;
-            pq.push({ncost, {nx, ny}});
-        }
-    }
-}
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cin >> MAP[i][j];
         }
     }
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             dp[i][j] = 2e9;
         }
     }
-    dp[1][1] = 0;
 
-    fc();
-    cout << dp[n][n];
+    dp[0][0] = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i > 0) dp[i][j] = min(dp[i][j], dp[i - 1][j]
+            + max(MAP[i][j] - MAP[i - 1][j] + 1, 0));
+            if (j > 0) dp[i][j] = min(dp[i][j], dp[i][j - 1]
+                                                + max(MAP[i][j] - MAP[i][j - 1] + 1, 0));
+        }
+    }
+    cout << dp[n - 1][n - 1];
 }
