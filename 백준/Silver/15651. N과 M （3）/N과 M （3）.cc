732e6175280d960d28ff770f647d45ff6de2ed0f
etakyung <161710171+xoruddl@users.noreply.github.com>
2024-11-25 01:52:36 +0900

    [Silver III] Title: N과 M (3), Time: 336 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+bool visited[10];
+vector<int> v;
+
+void fc(int cnt) {
+    if (cnt == M) {
+        for (int i = 0; i < M; i++) {
+            cout << v[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 1; i <= N; i++) {
+            visited[i] = true;
+            v.push_back(i);
+            fc(cnt + 1);
+            v.pop_back();
+            visited[i] = false;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    cin >> N >> M;
+
+    fc(0);
+}
