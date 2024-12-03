76a517e2f46ac98205c2ffa772d75d518a70db20
etakyung <161710171+xoruddl@users.noreply.github.com>
2024-12-04 00:32:13 +0900

    [Gold V] Title: 문자열 게임 2, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.cc
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.cc
@@ -1,15 +1,15 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-string W;
 int T, K;
+string W;
 vector<int> v[26];
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(0); cout.tie(0); cin.tie(0);
 
     cin >> T;
     while (T--) {
