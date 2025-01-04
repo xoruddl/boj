746d29ae6ff29e0eeeae75c80cea610370de1308
이태경 <161710171+xoruddl@users.noreply.github.com>
2025-01-04 15:22:54 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int T;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    cin >> T;
+    for (int t = 0; t < T; t++) {
+        int a, b;
+        cin >> a >> b;
+        cout << "yes\n";
+    }
+}
