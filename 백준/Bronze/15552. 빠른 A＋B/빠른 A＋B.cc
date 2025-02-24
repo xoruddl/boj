4443aee056fb767fd45bd720b19a53004028dda4
이태경 <161710171+xoruddl@users.noreply.github.com>
2025-02-24 11:10:13 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 296 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    for (int t = 0; t < T; t++) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a + b);
+
+    }
+}
