7489b472e4b649656fa0470f245c2e549a114043
이태경 <161710171+xoruddl@users.noreply.github.com>
2025-02-24 11:11:38 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 192 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -1,15 +1,15 @@
 #include <iostream>
-#include <cstdio>
 
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+
     int T;
-    scanf("%d", &T);
+    cin >> T;
     for (int t = 0; t < T; t++) {
         int a, b;
-        scanf("%d %d", &a, &b);
-        printf("%d\n", a + b);
-
+        cin >> a >> b;
+        cout << a + b << '\n';
     }
 }
