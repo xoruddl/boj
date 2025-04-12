12b057b593cff94891a18bb94bd4ef82ce39f5b3
이태경 <161710171+xoruddl@users.noreply.github.com>
2025-04-12 21:01:52 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 28 ms, Memory: 4000 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -5,25 +5,35 @@
 using namespace std;
 
 int N;
-int m[501][2];
-int dp[501][501];
+int dp[503][503];
+int m[503];
+int P[503][503];
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(0); cin.tie(0);
 
     cin >> N;
-    for (int i = 1; i <= N; ++i) {
-        cin >> m[i][0] >> m[i][1];
+    for (int i = 1; i <= N; i++) {
+        int a, b;
+        cin >> a >> b;
+        m[i - 1] = a;
+        m[i] = b;
     }
 
-    for (int i = 1; i < N; i++) {
-        for (int j = 1; i + j <= N; j++) {
-            dp[j][i + j] = INT_MAX;
-            for (int k = j; k <= i + j; k++) {
-                dp[j][i + j] = min(dp[j][i + j], dp[j][k] + dp[k + 1][i + j] + m[j][0] * m[k][1] * m[i + j][1]);
+    for (int diagonal = 1; diagonal <= N - 1; diagonal++) {
+        for (int i = 1; i <= N - diagonal; i++) {
+            int j = i + diagonal;
+            dp[i][j] = INT_MAX;
+
+            for (int k = i; k <= j - 1; k++) {
+                int cost = dp[i][k] + dp[k + 1][j] + m[i - 1] * m[k] * m[j];
+                if (cost < dp[i][j]) {
+                    dp[i][j] = cost;
+                    P[i][j] = k;
+                }
             }
         }
     }
+
     cout << dp[1][N];
-    return 0;
 }
