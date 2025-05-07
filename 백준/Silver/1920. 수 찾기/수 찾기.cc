3bf8ff69251968e8584d3a0df5ab1166a16aa258
이태경 <161710171+xoruddl@users.noreply.github.com>
2025-05-07 19:57:15 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -7,27 +7,6 @@ using namespace std;
 int N, M;
 vector<int> A;
 
-int binSearch(int n) {
-    int low = 0;
-    int high = A.size() - 1;
-
-    while (low <= high) {
-        int mid = (low + high) / 2;
-
-        if (n == A[mid]) {
-            return 1;
-        }
-        else if (n < A[mid]) {
-            high = mid - 1;
-        }
-        else if (n > A[mid]) {
-            low = mid + 1;
-        }
-    }
-
-    return 0;
-}
-
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
 
@@ -43,6 +22,6 @@ int main() {
         int num;
         cin >> num;
 
-        cout << binSearch(num) << '\n';
+        cout << binary_search(A.begin(), A.end(), num) << '\n';
     }
 }
