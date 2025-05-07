885e92cc55d758f831e546456183560d73f57baf
이태경 <161710171+xoruddl@users.noreply.github.com>
2025-05-07 19:43:55 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -5,7 +5,28 @@
 using namespace std;
 
 int N, M;
-vector<int> v1, v2;
+vector<int> A;
+
+int binSearch(int n) {
+    int low = 0;
+    int high = A.size() - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (n == A[mid]) {
+            return 1;
+        }
+        else if (n < A[mid]) {
+            high = mid - 1;
+        }
+        else if (n > A[mid]) {
+            low = mid + 1;
+        }
+    }
+
+    return 0;
+}
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
@@ -13,23 +34,15 @@ int main() {
     cin >> N;
     for (int i = 0; i < N; i++) {
         int a; cin >> a;
-        v1.push_back(a);
+        A.push_back(a);
     }
+    sort(A.begin(), A.end());
+
     cin >> M;
     for (int i = 0; i < M; i++) {
-        int a; cin >> a;
-        v2.push_back(a);
-    }
+        int num;
+        cin >> num;
 
-    sort(v1.begin(), v1.end());
-
-    for (int i = 0; i < M; i++) {
-        if (binary_search(v1.begin(),v1.end(),v2[i])) {
-            cout << 1 << '\n';
-        }
-        else {
-            cout << 0 << '\n';
-        }
+        cout << binSearch(num) << '\n';
     }
 }
-
