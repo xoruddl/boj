2df34385e1c2c80fc1e49a95a99ed1356295d9ef
이태경 <161710171+xoruddl@users.noreply.github.com>
2025-05-26 00:52:26 +0900

    [Gold IV] Title: 알파벳, Time: 392 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,48 +1,49 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-bool visited[27] = {false,};
 int R, C;
-char alphabet[21][21];
-int dx[] = {1, -1, 0, 0};
-int dy[] = {0, 0, 1, -1};
 int ans;
-
-void Input() {
-    cin >> R >> C;
-    for (int i = 0; i < R; i++){
-        for (int j = 0; j < C; j++){
-            cin >> alphabet[i][j];
-        }
-    }
-}
+vector<vector<int>> MAP;
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+bool visited[27];
 
 void dfs(int x, int y, int cnt) {
-    ans = max(ans, cnt);
+    ans = max(cnt, ans);
 
-    for (int i = 0; i < 4; i++){
+    for (int i = 0; i < 4; i++) {
         int nx = x + dx[i];
         int ny = y + dy[i];
 
-        if (nx >= 0 && ny >= 0 && nx < R && ny < C) {
-            if (visited[alphabet[nx][ny] - 'A'] == false) {
-                visited[alphabet[nx][ny] - 'A'] = true;
-                dfs(nx, ny, cnt + 1);
-                visited[alphabet[nx][ny] - 'A'] = false;
-            }
+        if (nx < 0 || nx >= C || ny < 0 || ny >= R) {
+            continue;
+        }
+        int next = MAP[ny][nx];
+        if (!visited[next]) {
+            visited[next] = true;
+            dfs(nx, ny, cnt + 1);
+            visited[next] = false;
         }
     }
 }
 
-int main()
-{
-    Input();
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
 
-    visited[alphabet[0][0] - 'A'] = true;
+    cin >> R >> C;
+    MAP.resize(R);
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            char c; cin >> c;
+            MAP[i].push_back(c - 'A');
+        }
+    }
+
+    visited[MAP[0][0]] = true;
     dfs(0, 0, 1);
 
     cout << ans;
-    return 0;
 }
