0bfb421445392913290c62302dcc8f2aad46fe51
이태경 <161710171+xoruddl@users.noreply.github.com>
2026-07-08 13:50:51 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,9 @@
+def solution(num):
+    answer = 0
+    while (num != 1):
+        if (num % 2 == 0):
+            num /= 2
+        else:
+            num = num * 3 + 1
+        answer += 1
+    return answer if answer < 500 else -1
