02dab61e52a59d3ed079c0eaba8fa19623f4b4e9
이태경 <161710171+xoruddl@users.noreply.github.com>
2026-07-09 10:03:14 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,7 +1,7 @@
 def gcd(a, b):
-    for i in range(min(a, b), 0, -1):
-        if (a % i == 0 and b % i == 0):
-            return i
+    if (b == 0):
+        return a
+    return gcd(b, a % b)
 
 def solution(n, m):
     ans = []
