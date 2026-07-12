381968c1c37a526bc73714c45eacb2e3ea9d6f77
이태경 <161710171+xoruddl@users.noreply.github.com>
2026-07-12 22:02:40 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.82 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,17 +1,8 @@
-from collections import deque
-
 def solution(food):
-    answer = ''
-    dq = deque()
-    dq.append(0)
-    
-    for i in range(len(food) - 1, -1, -1):
+    answer = '0'
+    for i in range(len(food) - 1, 0, -1):
         n = food[i] // 2
         while (n):
-            dq.append(i)
-            dq.appendleft(i)
+            answer = str(i) + answer + str(i)
             n -= 1
-    
-    print(dq)
-    ans = [str(i) for i in dq]
-    return "".join(ans)
+    return answer
