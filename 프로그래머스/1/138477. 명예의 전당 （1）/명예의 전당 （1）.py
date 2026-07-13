330682e14ce504d31252fdda3db928d8c453c8f3
이태경 <161710171+xoruddl@users.noreply.github.com>
2026-07-13 09:34:41 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.87 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,42 +1,11 @@
-from collections import deque
-
-answer = []
-
-def bin_insert(x, k):
-    global answer
-    l = -1
-    r = len(answer)
-    
-    while (l + 1 < r):
-        mid = (l + r) // 2
-        
-        if (answer[mid] < x):
-            l = mid
-        elif (answer[mid] > x):
-            r = mid
-        else:
-            r = mid
-            break
-        
-    answer.insert(r, x)
-    
-    if (len(answer) > k):
-        answer = answer[1:]
-    
-    # print(answer)
-    
-
 def solution(k, score):
-    global answer
-    answer.append(score[0])
-
-    n = len(score)
-    # li = [0, 300]
-    # li.insert(4, 400)
-    # print(li)
-    ans = [score[0]]
-    for i in range(1, n):
-        bin_insert(score[i], k)
-        ans.append(answer[0])
+    answer = []
+    temp = []
     
-    return ans
+    for i in range(len(score)):
+        temp.append(score[i])
+        
+        if (len(temp) > k):
+            temp.remove(min(temp))
+        answer.append(min(temp))
+    return answer
