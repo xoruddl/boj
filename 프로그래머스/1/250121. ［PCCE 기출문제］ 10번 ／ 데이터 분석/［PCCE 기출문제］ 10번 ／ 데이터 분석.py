34763ce68c8a6b99561be274c96d77e745384263
이태경 <161710171+xoruddl@users.noreply.github.com>
2026-07-26 13:08:57 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.08 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,21 +1,12 @@
 def solution(data, ext, val_ext, sort_by):
     answer = []
-    n = len(data);
-    targetIdx = 0;
+    columns = ["code", "date", "maximum", "remain"];
+    idx = columns.index(ext);
     
-    columns = ["code", "date", "maximum", "remain"]
+    for i in range(len(data)):
+        if (data[i][idx] < val_ext):
+            answer.append(data[i]);
     
-    for i in range(len(columns)):
-        if (columns[i] == sort_by):
-            targetIdx = i;
     
-    for i in range(n):
-        for j in range(4):
-            cur = data[i][j];
-            col = columns[j];
-            if (col == ext):
-                if (cur < val_ext):
-                    answer.append(data[i])
-    
-    answer.sort(key= lambda x: x[targetIdx])
+    answer.sort(key= lambda x: x[columns.index(sort_by)])
     return answer
