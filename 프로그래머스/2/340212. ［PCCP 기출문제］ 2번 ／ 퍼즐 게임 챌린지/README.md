1dfb25476ae138e85214d44fb9f8bb414252c878
이태경 <161710171+xoruddl@users.noreply.github.com>
2026-08-03 14:42:18 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 13.08 ms, Memory: 23.6 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/README.md
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지 - 340212 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340212) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340212?language=cpp) 
 
 ### 성능 요약
 
-메모리: 36.1 MB, 시간: 1116.01 ms
+메모리: 23.6 MB, 시간: 13.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 03일 14:16:54
+2026년 08월 03일 14:42:10
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+
+bool fc(vector<int>& diffs, vector<int>& times, ll limit, ll mid){
+    ll total = 0;
+    ll time_prev = 0;
+    
+    for (int i = 0; i < diffs.size(); i++) {
+        ll gap = mid - diffs[i];
+        ll time_cur = times[i];
+        
+        if (gap >= 0){
+            total += time_cur;
+        } else {
+            total += (-gap) * (time_cur + time_prev) + time_cur;
+        }
+        time_prev = time_cur;
+    }
+    
+    cout << total << '\n';
+    if (total <= limit) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+double solution(vector<int> diffs, vector<int> times, ll limit) {
+    
+    ll l = 0;
+    ll r = 1000000000000000 + 1;
+    
+    while (l + 1 < r){
+        ll mid = (l + r) / 2;
+        
+        // 레벨을 낮춰도 됨
+        if (fc(diffs, times, limit, mid)){
+            r = mid;
+        } else {
+            // 해당 레벨보다는 높아야함.
+            l = mid;
+        }
+    }
+    
+    return r;
+}
