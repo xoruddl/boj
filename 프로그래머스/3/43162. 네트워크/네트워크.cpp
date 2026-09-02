1beb130ac3bf710e6135b7a32c27793aa894a565
이태경 <161710171+xoruddl@users.noreply.github.com>
2026-09-02 21:33:02 +0900

    [level 3] Title: 네트워크, Time: 0.04 ms, Memory: 4.58 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,39 +1,42 @@
 #include <string>
 #include <vector>
+#include <iostream>
 #include <queue>
 
 using namespace std;
 
-#define MAX 203
+bool visited[203];
+vector<vector<int>> graph;
 
-bool visited[MAX];
-queue<int> q;
-
-void bfs(int node, const vector<vector<int>> &nodes) {
-    visited[node] = true;
-    q.push(node);
+void bfs(int x) {
+    visited[x] = true;
+    queue<int> q;
+    q.push(x);
     
     while (!q.empty()) {
         int cur = q.front();
         q.pop();
         
-        for (int i = 0; i < nodes[cur].size(); i++) {
-            int next = i;
-            if (visited[next] || nodes[cur][i] == 0) {
-                continue;
+        for (int i = 0; i < graph[cur].size(); i++) {
+            int num = graph[cur][i];
+            if (num == 1) {
+                if (!visited[i]) {
+                    q.push(i);
+                    visited[i] = true;
+                }
             }
-            q.push(next);
-            visited[next] = true;
         }
     }
 }
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
+    graph = computers;
+
     
     for (int i = 0; i < n; i++) {
         if (!visited[i]) {
-            bfs(i, computers);
+            bfs(i);
             answer++;
         }
     }
